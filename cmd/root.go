ac22d70d66d0c0feb1c3b69dc78132e8c4097461
Iris Scholten <ischolten.is@gmail.com>
2018-07-19 10:18:37 -0700

    feat: add cli skeleton including root and version commands (#14)
    * feat: add cli skeleton including root and version commands
    
    Co-authored-by: Jared Scheib <jared.scheib@gmail.com>
    
    * fix: comment description and restore welcome message
    
    Co-authored-by: Jared Scheib <jared.scheib@gmail.com>

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "codenames",
+	Short: "Codenames is a game",
+	Run: func(cmd *cobra.Command, arg []string) {
+		fmt.Println("Welcome to Codenames")
+	},
+}
+
+// Execute runs the root cobra command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
