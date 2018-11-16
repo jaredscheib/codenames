caeab96ee5ac0b358dfc8fb7c41fc9a9ae699d39
Jared Scheib <jared.scheib@gmail.com>
2018-11-16 09:31:59 -0800

    feat: introduce view cmd for spymaster vs player view
    Co-authored-by: Iris Scholten <ischolten.is@gmail.com>

--- cmd/view.go
+++ cmd/view.go
@@ -8,15 +8,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var startCmd = &cobra.Command{
-	Use: "start",
+var viewCmd = &cobra.Command{
+	Use: "view",
 	Run: func(cmd *cobra.Command, args []string) {
-		g := server.NewGame()
-		p := cli.RenderTurnPrompt(g.WhoseTurn)
+		g := server.NewGame() // TODO: make this the current game
+		p := cli.RenderField(g, args[0])
 		fmt.Println(p)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(viewCmd)
 }
